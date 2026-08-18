4aafdb051a21faf6ab5e4b0d18eda17fc5947917
MSGO0303 <gmdohci@gmail.com>
2026-08-18 17:37:54 +0900

    fix-hydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ko" className="bg-background">
+    <html lang="ko" className="bg-background" suppressHydrationWarning>
       <body className="antialiased">
         {children}
         {process.env.NODE_ENV === 'production' && <Analytics />}
